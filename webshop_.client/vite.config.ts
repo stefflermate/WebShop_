fc0cf4d5369ad5ddb23a3ff1c87a554fc550aaab
stefflermate <steffler.mate2@gmail.com>
2025-03-26 09:36:04 +0100

    mükodik headben

--- webshop_.client/vite.config.ts
+++ webshop_.client/vite.config.ts
@@ -66,7 +66,8 @@ export default defineConfig(({ mode }) => {
                 key: fs.readFileSync(keyFilePath),
                 cert: fs.readFileSync(certFilePath),
             }
-        }
+        },
+        
         
     };
 });
